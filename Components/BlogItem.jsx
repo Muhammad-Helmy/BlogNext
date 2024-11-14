baa5278a497e4e62ac4b94b489e5032a182a1037
Muhammad Helmy Fadillah <helmy.fadillah.work@gmail.com>
2024-11-15 01:41:29 +0700

    nyoba

---
+++ Components/BlogItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+import {assets} from '@/Assets/assets'
+
+
+const BlogItem = () => {
+  return (
+    <div className="max-w-[330px] sm:maz-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_black]">
+       <Image src/>                  
+    </div>
+  )
+}
+
+export default BlogItem
