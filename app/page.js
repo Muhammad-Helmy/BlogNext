b492a61a53635bc9a5b3888cd85b8d181f59d0d2
Muhammad Helmy Fadillah <helmy.fadillah.work@gmail.com>
2024-11-15 18:49:31 +0700

    try to fix css problem

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 'use client'
 import Header from "@/Components/Header";
 import BlogList from "@/Components/BlogList";
+import Footer from "@/Components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header/>
       <BlogList/>
+      <Footer/>
     </>
   );
 }
